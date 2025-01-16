ad4ea79890dc000d72f169fcc5c5561f1182f748
David Cotter <daveguy28@gmail.com>
2025-01-16 20:06:53 +0000

    update

--- trafficking_simulator/settings.py
+++ trafficking_simulator/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'simulator',
+    'results',
+    'resources',
+    'about',
 ]
 
 MIDDLEWARE = [
