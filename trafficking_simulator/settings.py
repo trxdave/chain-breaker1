9384469e27e06951d4e92390d6642b3c577766cf
David Cotter <daveguy28@gmail.com>
2025-01-17 10:10:53 +0000

    about page create

--- trafficking_simulator/settings.py
+++ trafficking_simulator/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#my+pxp(lw4l&00i7uj-qgyq&nk$q6&+7rpw%c2y)vrn&5g22#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'trafficking_simulator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR], 
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
