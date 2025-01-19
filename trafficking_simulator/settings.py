895eec556f64298466a30e22624ee0100310470b
David Cotter <daveguy28@gmail.com>
2025-01-19 19:50:05 +0000

    collectstatic

--- trafficking_simulator/settings.py
+++ trafficking_simulator/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-trxdave-chainbreaker1-7esv1203v6r.ws.codeinstitute-ide.net',
